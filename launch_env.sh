99fd15ce69ce5cb8f31a96dddbf8254b1c136831
Mr.one <mawei1991@gmail.com>
2025-10-28 10:14:12 +0800

    编辑launch_env.sh

--- launch_env.sh
+++ launch_env.sh
@@ -5,8 +5,9 @@ export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
-export ATHENA_HOST='ws://server.mr-one.cn:7899'
-export API_HOST='http://server.mr-one.cn:7898'
+export ATHENA_HOST='ws://op.mr-one.cn:7899'
+export API_HOST='http://op.mr-one.cn:7898'
+
 
 
 if [ -z "$AGNOS_VERSION" ]; then
